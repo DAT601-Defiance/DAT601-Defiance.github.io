238fbbc2a1487afb96a005e1136ff97d93bd1181
KyralnBoyle <kyra.boyle@students.plymouth.ac.uk>
2021-01-12 17:21:57 +0000

    update

--- js/sketch.js
+++ js/sketch.js
@@ -1,63 +1,42 @@
-//
-document.addEventListener("contextmenu", function (e) {
-        e.preventDefault();
-    }, false);
-
+let eraseEnable = false;
 //Assigning the variabels
-var black;
-var alphaC;
-var layer;
-var rad = 100;
-//starting the mouse as false
-var mouseIsDragged = false;
+var vid;
 
 //inital setup
 function setup() {
   //Lets the canvas be the size of the screening causing it to be resonsive
   var canvas = createCanvas(windowWidth, windowHeight);
-  //Allows the P5.js code to show in the HTML
   canvas.parent("canvasContainer");
-  //Defining that W and H should also be responsive to the window size
-  var w = windowWidth, h = windowHeight;
-  title = text("Welcome", width/2, height/2);
-  pixelDensity(1);
-  black = color(400);
-  alphaC = color(0,0);
-  //this creates the layer that goes over the top of the background
-  layer = createGraphics(w, h);
-  background(220);
-  layer.fill("green");
-  layer.rect(0,0,width,height);
+
+  vid= createVideo(['assets/img/Code.mp4'],vidLoad);
+  vid.hide();
+
+  toggleBtn = createButton("Toggle erase");
+  toggleBtn.position(30, 60);
+  toggleBtn.mouseClicked(toggleErase);
+}
+
+function mouseIsPressed() {
+  fill('blue');
+  noStroke();
+  circle(mouseX, mouseY, 50);
 }
 
 function draw() {
-  noStroke();
-  for (var i = 0; i < 10; i++) {
-    fill(i*25);
-    rect(i*width/10, 0, width/10, height)
-  }
-  image(layer, 0, 0);
+  image(vid, 1, 1); // draw the video frame to canvas
+}
 
-  if (mouseIsPressed) {
-    noFill();
-    stroke("gray");
-    ellipse(mouseX, mouseY, rad*2, rad*2);
-    line(mouseX - 10, mouseY, mouseX+10, mouseY);
-    line(mouseX, mouseY-10, mouseX, mouseY+10);
+function toggleErase() {
+  if (eraseEnable) {
+    noErase();
+    eraseEnable = false;
+  }
+  else {
+    erase();
+    eraseEnable = true;
   }
 }
 
-function mouseDragged() {
-  for (var x = mouseX - rad; x < mouseX+rad; x++) {
-    for (var y = mouseY - rad; y < mouseY+rad; y++) {
-      if ((dist(x,y, mouseX, mouseY) < rad) && x > 0 && x <= width) {
-        layer.set(x,y,alphaC);
-      }
-    }
-  }
-  layer.updatePixels();
-}
-
-function thanks(){
-
+function vidLoad() {
+  vid.loop();
 }
